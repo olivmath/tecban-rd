2f5ac5b3f029ccfd806b2b37e5f297f90bd3ec67
Bujira <yurifcorrea@gmail.com>
2023-11-03 18:03:22 +0100

    feat: approve burn dto

--- src/dtos/real-tokenizado.dto.ts
+++ src/dtos/real-tokenizado.dto.ts
@@ -83,3 +83,27 @@ export class RealTokenizadoInternalTransferDTO {
   @IsNotEmpty()
   amount: number;
 }
+
+export class RealTokenizadoApproveBurnDTO {
+  @ApiProperty({
+    description: 'Descrição da interação com o contrato',
+    default: '',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty({
+    description: 'ID que identifica o ativo na Parfin',
+    example: '01574a14-25bc-45f6-aaf6-cd71a60b1d35',
+  })
+  @IsString()
+  @IsNotEmpty()
+  assetId: string;
+
+  @ApiProperty({ description: 'Quantidade uint256 a ser queimada' })
+  @IsNumber()
+  @IsNotEmpty()
+  amount: number;
+}
