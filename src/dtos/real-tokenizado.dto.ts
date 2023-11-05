0a437a49f45cd94481460809b4f93fbff59cdb99
Bujira <yurifcorrea@gmail.com>
2023-11-05 11:41:24 +0100

    feat: approve burn dto

--- src/dtos/real-tokenizado.dto.ts
+++ src/dtos/real-tokenizado.dto.ts
@@ -55,6 +55,65 @@ export class RealTokenizadoBurnDTO {
   amount: number;
 }
 
+export class RealTokenizadoBurnFromDTO {
+  @ApiProperty({
+    description: 'Descrição da interação com o contrato',
+    default: '',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty({
+    description: 'ID que identifica o ativo na Parfin',
+    example: '01574a14-25bc-45f6-aaf6-cd71a60b1d35',
+  })
+  @IsString()
+  @IsNotEmpty()
+  assetId: string;
+
+  @ApiProperty({
+    description: 'Endereço da conta onde será feito o resgate',
+    example: '0xc0ffee254729296a45a3885639AC7E10F9d54979',
+  })
+  @IsString()
+  @IsNotEmpty()
+  account: string;
+
+  @ApiProperty({
+    description: 'Quantidade uint256 a ser queimada',
+    example: 1000
+  })
+  @IsString()
+  @IsNotEmpty()
+  amount: string;
+}
+
+export class RealTokenizadoApproveBurnDTO {
+  @ApiProperty({
+    description: 'Descrição da interação com o contrato',
+    default: '',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty({
+    description: 'ID que identifica o ativo na Parfin',
+    example: '01574a14-25bc-45f6-aaf6-cd71a60b1d35',
+  })
+  @IsString()
+  @IsNotEmpty()
+  assetId: string;
+
+  @ApiProperty({ description: 'Quantidade uint256 a ser queimada' })
+  @IsString()
+  @IsNotEmpty()
+  amount: string;
+}
+
 export class RealTokenizadoInternalTransferDTO {
   @ApiProperty({
     description: 'Descrição da interação com o contrato',
@@ -79,30 +138,6 @@ export class RealTokenizadoInternalTransferDTO {
   key: string;
 
   @ApiProperty({ description: 'Quantidade uint256 a ser transferida' })
-  @IsNumber()
-  @IsNotEmpty()
-  amount: number;
-}
-
-export class RealTokenizadoApproveBurnDTO {
-  @ApiProperty({
-    description: 'Descrição da interação com o contrato',
-    default: '',
-    required: false,
-  })
-  @IsString()
-  @IsOptional()
-  description?: string;
-
-  @ApiProperty({
-    description: 'ID que identifica o ativo na Parfin',
-    example: '01574a14-25bc-45f6-aaf6-cd71a60b1d35',
-  })
-  @IsString()
-  @IsNotEmpty()
-  assetId: string;
-
-  @ApiProperty({ description: 'Quantidade uint256 a ser queimada' })
   @IsNumber()
   @IsNotEmpty()
   amount: number;
