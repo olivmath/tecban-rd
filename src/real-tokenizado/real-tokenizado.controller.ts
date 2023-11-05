e19c0cd24d77f2561c51392de9ccc0ecc244b35f
Bujira <yurifcorrea@gmail.com>
2023-11-05 11:41:43 +0100

    feat: approve burn controller

--- src/real-tokenizado/real-tokenizado.controller.ts
+++ src/real-tokenizado/real-tokenizado.controller.ts
@@ -5,6 +5,7 @@ import {
   RealTokenizadoBurnDTO,
   RealTokenizadoInternalTransferDTO,
   RealTokenizadoApproveBurnDTO,
+  RealTokenizadoBurnFromDTO,
 } from '../dtos/real-tokenizado.dto';
 import { RealTokenizadoService } from './real-tokenizado.service';
 
@@ -25,6 +26,18 @@ export class RealTokenizadoController {
     return this.realTokenizadoService.burn(dto);
   }
 
+  @Post('burn-from')
+  @ApiOperation({ summary: 'Burn Real Tokenizado from a wallet', description: 'Burn Real Tokenizado token from a different wallet' })
+  burnFrom(@Body() dto: RealTokenizadoBurnFromDTO) {
+    return this.realTokenizadoService.burnFrom(dto);
+  }
+
+  @Post('approve-burn')
+  @ApiOperation({ summary: 'Approve Real Tokenizado Burn', description: 'Approve a Real Tokenizado Burn transaction' })
+  approveBurn(@Body() dto: RealTokenizadoApproveBurnDTO) {
+    return this.realTokenizadoService.approveBurn(dto);
+  }
+
   @Post('internal-transfer')
   @ApiOperation({ summary: 'Tranfer Real Tokenizado (Internal)', description: 'Transfer Real Tokenizado token between clients from the same financial institution' })
   transfer(@Body() dto: RealTokenizadoInternalTransferDTO,) {
@@ -36,11 +49,5 @@ export class RealTokenizadoController {
   @ApiParam({ name: 'address', example: '0x5be4C55e1977E555DB9a815a2CDed576A71Ca3c2' })
   balanceOf(@Param('address') address: string) {
     return this.realTokenizadoService.balanceOf(address);
-  }
-
-  @Post('approve-burn')
-  @ApiOperation({ summary: 'Approve Real Tokenizado Burn', description: 'Approve a Real Tokenizado Burn transaction' })
-  approveBurn(@Body() dto: RealTokenizadoApproveBurnDTO) {
-    return this.realTokenizadoService.approveBurn(dto);
   }
 }
