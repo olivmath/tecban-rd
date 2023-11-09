063ca71e533ab32a67cb91b8de8d784af8f09b46
Lucas Oliveira <olivmath@protonmail.com>
2023-11-09 17:08:08 -0300

    update: real digital tests

--- src/real-digital/test/real-digital.controller.spec.ts
+++ src/real-digital/test/real-digital.controller.spec.ts
@@ -7,12 +7,12 @@ import { LoggerService } from 'src/logger/logger.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { TransactionsModule } from 'src/transactions/transactions.module';
-import { ParfinAuth } from 'src/auth/parfin.auth';
-import { parfinApi } from 'src/config/parfin-api-client';
+import { ParfinHttpService } from 'src/parfin/parfin.api.service';
+import { AxiosResponse } from 'axios';
 
 describe('RealDigitalController', () => {
     let controller: RealDigitalController;
-    let service: RealDigitalService;
+    let service: ParfinHttpService;
     let mongod: MongoMemoryServer;
 
     beforeAll(async () => {
@@ -23,11 +23,11 @@ describe('RealDigitalController', () => {
         const module: TestingModule = await Test.createTestingModule({
             imports: [MongooseModule.forRoot(uri), TransactionsModule],
             controllers: [RealDigitalController],
-            providers: [RealDigitalService, ParfinService, ContractHelperService, LoggerService, ParfinAuth],
+            providers: [RealDigitalService, ParfinService, ContractHelperService, LoggerService, ParfinHttpService],
         }).compile();
 
         controller = module.get<RealDigitalController>(RealDigitalController);
-        service = module.get<RealDigitalService>(RealDigitalService);
+        service = module.get<ParfinHttpService>(ParfinHttpService);
     });
 
     afterAll(async () => {
@@ -39,30 +39,42 @@ describe('RealDigitalController', () => {
             realDigitalBalanceOf: 1,
             realDigitalFrozenBalanceOf: 0,
         };
-        // Mock `parfinApi.post` when address discovery call
-        jest.spyOn(parfinApi, 'post').mockResolvedValueOnce(
+        // Mock `ParfinHttpService.makeRequest` when address discovery call
+        jest.spyOn(service, 'makeRequest').mockResolvedValueOnce(
             Promise.resolve({
                 data: {
                     data: '0x00000000000000000000000060c48562056c6cfcd2128ce60fd18c67e81ed971',
                 },
-            }),
+                status: 200,
+                statusText: 'OK',
+                headers: {},
+                config: {},
+            } as AxiosResponse),
         );
-        // Mock `parfinApi.post` when real digital balance of call
-        jest.spyOn(parfinApi, 'post').mockResolvedValueOnce(
+        // Mock `ParfinHttpService.makeRequest` when real digital balance of call
+        jest.spyOn(service, 'makeRequest').mockResolvedValueOnce(
             Promise.resolve({
                 data: {
                     data: '0x0000000000000000000000000000000000000000000000000000000000000064',
                 },
-            }),
+                status: 200,
+                statusText: 'OK',
+                headers: {},
+                config: {},
+            } as AxiosResponse),
         );
 
-        // Mock `parfinApi.post` when real digital frozen balance of call
-        jest.spyOn(parfinApi, 'post').mockResolvedValueOnce(
+        // Mock `ParfinHttpService.makeRequest` when real digital frozen balance of call
+        jest.spyOn(service, 'makeRequest').mockResolvedValueOnce(
             Promise.resolve({
                 data: {
                     data: '0x0000000000000000000000000000000000000000000000000000000000000100',
                 },
-            }),
+                status: 200,
+                statusText: 'OK',
+                headers: {},
+                config: {},
+            } as AxiosResponse),
         );
 
         const address = '0x5be4C55e1977E555DB9a815a2CDed576A71Ca3c2';
