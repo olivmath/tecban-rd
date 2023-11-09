bb2127173b76ddd60b70c33ac5c2beed051e1bd5
Lucas Oliveira <olivmath@protonmail.com>
2023-11-09 14:24:57 -0300

    add: middleware, filter, interceptors in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
@@ -9,6 +9,12 @@ import { ParfinModule } from './parfin/parfin.module';
 import { RealDigitalModule } from './real-digital/real-digital.module';
 import { RealTokenizadoModule } from './real-tokenizado/real-tokenizado.module';
 import { ContractHelperModule } from './helpers/contract-helper/contract-helper.module';
+import { LoggerModule } from './logger/logger.module';
+import { AllExceptionsFilter } from './filters/http-exception.filter';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import { RequestIdMiddleware } from './middleware/request-id.middleware';
+import { RequestIdResponseInterceptor } from './interceptors/response.request-id.interceptor';
 
 @Module({
     imports: [
@@ -21,8 +27,23 @@ import { ContractHelperModule } from './helpers/contract-helper/contract-helper.
         TransactionsModule,
         WalletModule,
         ContractHelperModule,
+        LoggerModule,
+    ],
+    providers: [
+        {
+            provide: APP_FILTER,
+            useClass: AllExceptionsFilter,
+        },
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: RequestIdResponseInterceptor,
+        },
     ],
 
     controllers: [HealthController],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(RequestIdMiddleware, LoggerMiddleware).forRoutes('*');
+    }
+}
