49abce6c405fd43d6e101de4c52b8b6a09ebc59a
Lucas Oliveira <olivmath@protonmail.com>
2023-11-14 01:55:21 -0300

    add: webhook dto

---
+++ src/webhook/dto/customer-smart-contract-events.dto.ts
@@ -0,0 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CustomerSmartContractEventsDto {
+    @ApiProperty({ description: 'Unique identifier of the event' })
+    Id: string;
+
+    @ApiProperty({ description: 'Customer ID' })
+    CustomerId: string;
+
+    @ApiProperty({ description: 'Creation date of the event' })
+    CreatedAt: string;
+
+    @ApiProperty({ description: 'Name of the event' })
+    Event: string;
+
+    @ApiProperty({ description: 'Blockchain identifier' })
+    BlockchainId: string;
+
+    @ApiProperty({ description: 'Last time the event was updated' })
+    LastTimeUpdated: string;
+
+    @ApiProperty({ description: 'Log index as a string' })
+    LogIndex: string;
+
+    @ApiProperty({ description: 'Trace key for the event' })
+    TraceKey: string;
+
+    @ApiProperty({ description: 'Smart contract address' })
+    SmartContract: string;
+
+    @ApiProperty({ description: 'Transaction hash associated with the event' })
+    TransactionHash: string;
+}
