7efe453eec1501e345336689d227b55f4ed5eba5
Lucas Oliveira <olivmath@protonmail.com>
2023-11-14 01:55:06 -0300

    add: middleware for validate signature of webhook

---
+++ src/middleware/webhook-validation.middleware.ts
@@ -0,0 +1,27 @@
+import { Injectable, NestMiddleware, BadRequestException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import * as crypto from 'crypto';
+import { AppError } from 'src/error/app.error';
+import { LoggerService } from 'src/logger/logger.service';
+
+@Injectable()
+export class WebhookValidationMiddleware implements NestMiddleware {
+    constructor(private readonly logger: LoggerService) {}
+    use(req: Request, res: Response, next: NextFunction): void {
+        const signature = req.headers['x-webhook-signature'] as string;
+        const secret = process.env.PARFIN_PRIVATE_KEY;
+        const payload = JSON.stringify(req.body);
+
+        if (!this.isValidSignature(signature, payload, secret)) {
+            throw new AppError(401, 'Invalid webhook signature');
+        }
+
+        
+        next();
+    }
+    
+    private isValidSignature(signature: string, payload: string, secret: string): boolean {
+        const hash = crypto.createHmac('sha256', secret).update(payload).digest('hex');
+        return hash === signature;
+    }
+}
