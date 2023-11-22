2c05b5efa109480fdf192afbb5d919a0c120f55e
Bujira <yurifcorrea@gmail.com>
2023-11-22 19:00:57 +0100

    feat: set approval dto

--- src/dtos/tpft.dto.ts
+++ src/dtos/tpft.dto.ts
@@ -1,14 +1,45 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
-export class TPFtGetBalanceOfBody {
-  @ApiProperty({ description: 'ID do TPFt (LTN, LFT, etc.)' })
+export class TPFtSetApprovalForAllDTO {
+  @ApiProperty({
+    description: 'Descrição da interação com o contrato',
+    default: '',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty({ description: 'ID que identifica a carteira na Parfin', })
+  @IsString()
+  @IsNotEmpty()
+  walletAddress: string;
+
+  @ApiProperty({ description: 'ID que identifica o ativo na Parfin' })
+  @IsString()
+  @IsNotEmpty()
+  assetId: string;
+
+  @ApiProperty({ description: 'Endereço do contrato que irá manipular TPFt' })
+  @IsString()
+  @IsNotEmpty()
+  operator: string;
+
+  @ApiProperty({ description: 'Aprovar ou não a manipulação de TPFt do operador' })
+  @IsBoolean()
+  @IsNotEmpty()
+  approved: boolean;
+}
+
+export class TPFtGetBalanceOfQuery {
+  @ApiProperty({ description: 'ID do TPFt (1, 2, 3, etc.)' })
   @IsString()
   @IsNotEmpty()
   tpftID: string;
 }
 
-export class TPFtGetBalanceOfDTO extends TPFtGetBalanceOfBody {
+export class TPFtGetBalanceOfDTO extends TPFtGetBalanceOfQuery {
   @ApiProperty({ description: 'Endereço da carteira' })
   @IsString()
   @IsNotEmpty()
