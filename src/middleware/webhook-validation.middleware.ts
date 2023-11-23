49f7164ae7d424fe6d8428c7b3631c8ec715116d
Lucas Oliveira <olivmath@protonmail.com>
2023-11-23 14:38:05 -0300

    fix: webhook signature validations and add request id in new app error creation

--- src/middleware/webhook-validation.middleware.ts
+++ src/middleware/webhook-validation.middleware.ts
@@ -9,18 +9,19 @@ export class WebhookValidationMiddleware implements NestMiddleware {
     constructor(private readonly logger: LoggerService) {}
     use(req: Request, res: Response, next: NextFunction): void {
         const signature = req.headers['x-webhook-signature'] as string;
-        const secret = process.env.PARFIN_PRIVATE_KEY;
-        const payload = JSON.stringify(req.body);
 
-        this.isValidSignature(signature, payload, secret);
+        const hash = crypto
+            .createHmac('sha256', process.env.PARFIN_PRIVATE_KEY)
+            .update(JSON.stringify(req.body))
+            .digest('hex');
+
+        if (hash !== signature) {
+            this.logger.warn(`client signature: ${signature}`)
+            this.logger.warn(`server signature: ${hash}`)
+            const requestId = req['requestId'] || req.headers['x-request-id'];
+            throw new AppError(401, 'Invalid webhook signature', { requestId });
+        }
 
         next();
-    }
-
-    private isValidSignature(signature: string, payload: string, secret: string) {
-        const hash = crypto.createHmac('sha256', secret).update(payload).digest('hex');
-        if (hash !== signature) {
-            throw new AppError(401, 'Invalid webhook signature');
-        }
     }
 }
