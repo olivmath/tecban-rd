3b0edcc8b4cfb1bd0ba5b32799821c0849aac25e
Bujira <yurifcorrea@gmail.com>
2023-11-24 21:05:47 +0100

    chore: tpft swagger responses update

--- src/res/swagger/tpft.swagger.ts
+++ src/res/swagger/tpft.swagger.ts
@@ -1,11 +1,15 @@
 import { ApiResponse } from "@nestjs/swagger";
-import { TPFtGetBalanceOfSuccessRes, TPFtTradeRes } from "../app/tpft.responses";
+import { TPFtGetBalanceOfSuccessRes, TPFtBuyRes, TPFtSellRes } from "../app/tpft.responses";
 
 // Success
 export const getTpftBalance201 = ApiResponse({
   status: 201, description: 'Successful operation.', type: TPFtGetBalanceOfSuccessRes
 });
 
-export const tradeTpft201 = ApiResponse({
-  status: 201, description: 'Successful operation.', type: TPFtTradeRes
+export const buyTpft201 = ApiResponse({
+  status: 201, description: 'Successful operation.', type: TPFtBuyRes
+});
+
+export const sellTpft201 = ApiResponse({
+  status: 201, description: 'Successful operation.', type: TPFtSellRes
 });
