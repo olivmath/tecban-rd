d87b5d54fe15c0ca8d094a2c083a9e0428923d90
Bujira <yurifcorrea@gmail.com>
2023-11-24 14:18:04 +0100

    feat: buy and sell swagger res

--- src/res/swagger/tpft.swagger.ts
+++ src/res/swagger/tpft.swagger.ts
@@ -1,7 +1,11 @@
 import { ApiResponse } from "@nestjs/swagger";
-import { TPFtGetBalanceOfSuccessRes } from "../app/tpft.responses";
+import { TPFtGetBalanceOfSuccessRes, TPFtTradeRes } from "../app/tpft.responses";
 
 // Success
 export const getTpftBalance201 = ApiResponse({
   status: 201, description: 'Successful operation.', type: TPFtGetBalanceOfSuccessRes
+});
+
+export const tradeTpft201 = ApiResponse({
+  status: 201, description: 'Successful operation.', type: TPFtTradeRes
 });
