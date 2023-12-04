a0573aa678f89a0cb8917819ec8e89d4219e8831
Bujira <yurifcorrea@gmail.com>
2023-12-04 21:21:53 +0100

    chore: updating config service

--- config/config.service.ts
+++ config/config.service.ts
@@ -11,36 +11,53 @@ export class ConfigService {
     SWAGGER_URL: string;
     LOG: string;
 
+    // Informações sobre os usuários da aplicação
     ARBI_USER_ID: string;
     ARBI_CLIENT_1_USER_ID: string;
     ARBI_CLIENT_2_USER_ID: string;
 
+    // Credenciais da Parfin
     PARFIN_API_V1_URL_SANDBOX: string;
     BLOCKCHAIN_ID: string;
-    PARFIN_WALLET_ID: string;
     API_KEY: string;
     API_SECRET: string;
     ISSUER: string;
     AUDIENCE: string;
     PARFIN_PRIVATE_KEY: string;
 
+    // Informações das carteiras dos usuários
+    // - Banco Arbi
     ARBI_DEFAULT_WALLET_ADDRESS: string;
     ARBI_RD_ASSET_ID: string;
     ARBI_RT_ASSET_ID: string;
+    BACEN_DEFAULT_ASSET_ID: string;
+    // - Cliente 1 do Banco Arbi
     ARBI_CLIENT_1_WALLET_ADDRESS: string;
     ARBI_CLIENT_1_RT_ASSET_ID: string;
+    ARBI_CLIENT_1_BACEN_ASSET_ID: string;
+    // - Cliente 2 do Banco Arbi
     ARBI_CLIENT_2_WALLET_ADDRESS: string;
     ARBI_CLIENT_2_RT_ASSET_ID: string;
+    ARBI_CLIENT_2_BACEN_ASSET_ID: string;
+    // - Cliente 3 do Banco Arbi
+    ARBI_CLIENT_3_WALLET_ADDRESS: string;
+    ARBI_CLIENT_3_RT_ASSET_ID: string;
+    ARBI_CLIENT_3_BACEN_ASSET_ID: string;
 
+    // CNPJ8 dos participantes
     STN_CNPJ8: string;
     BRADESCO_CNPJ8: string;
     ABC_CNPJ8: string;
     INTER_CNPJ8: string;
     ITAU_CNPJ8: string;
     RIBEIRAO_PRETO_CNPJ8: string;
+
+    // Informações dos TPFts
     TPFT_OPERATION_ID: string;
     LTN_ID: string;
     LFT_ID: string;
+
+    // Endereço dos contratos do piloto
     ADDRESS_DISCOVERY_ADDRESS: string;
     TPFT_ADDRESS: string;
     TPFT_1002_ADDRESS: string;
@@ -50,7 +67,7 @@ export class ConfigService {
     REAL_DIGITAL_ADDRESS: string;
     REAL_DIGITAL_DEFAULT_ACCOUNT_ADDRESS: string;
     REAL_DIGITAL_ENABLE_ACCOUNT_ADDRESS: string;
-    ARBI_REAL_TOKENIZADO_ADDRESS: string;
+    REAL_TOKENIZADO_ARBI_ADDRESS: string;
     STR_ADDRESS: string;
     SWAP_ONE_STEP_ADDRESS: string;
     SWAP_ONE_STEP_FROM_ADDRESS: string;
@@ -64,7 +81,7 @@ export class ConfigService {
             const value = process.env[varName];
 
             if (!value) {
-              
+
                 throw new AppError(500, `A aplicação não pode iniciar sem a variável: '${varName}'`);
             }
 
@@ -82,7 +99,6 @@ export class ConfigService {
 
         this.PARFIN_API_V1_URL_SANDBOX = ensure('PARFIN_API_V1_URL_SANDBOX');
         this.BLOCKCHAIN_ID = ensure('BLOCKCHAIN_ID');
-        this.PARFIN_WALLET_ID = ensure('PARFIN_WALLET_ID');
         this.API_KEY = ensure('API_KEY');
         this.API_SECRET = ensure('API_SECRET');
         this.ISSUER = ensure('ISSUER');
@@ -92,10 +108,16 @@ export class ConfigService {
         this.ARBI_DEFAULT_WALLET_ADDRESS = ensure('ARBI_DEFAULT_WALLET_ADDRESS');
         this.ARBI_RD_ASSET_ID = ensure('ARBI_RD_ASSET_ID');
         this.ARBI_RT_ASSET_ID = ensure('ARBI_RT_ASSET_ID');
+        this.BACEN_DEFAULT_ASSET_ID = ensure('ACEN_DEFAULT_ASSET_ID');
         this.ARBI_CLIENT_1_WALLET_ADDRESS = ensure('ARBI_CLIENT_1_WALLET_ADDRESS');
         this.ARBI_CLIENT_1_RT_ASSET_ID = ensure('ARBI_CLIENT_1_RT_ASSET_ID');
+        this.ARBI_CLIENT_1_BACEN_ASSET_ID = ensure('ARBI_CLIENT_1_BACEN_ASSET_ID');
         this.ARBI_CLIENT_2_WALLET_ADDRESS = ensure('ARBI_CLIENT_2_WALLET_ADDRESS');
         this.ARBI_CLIENT_2_RT_ASSET_ID = ensure('ARBI_CLIENT_2_RT_ASSET_ID');
+        this.ARBI_CLIENT_2_BACEN_ASSET_ID = ensure('ARBI_CLIENT_2_BACEN_ASSET_ID');
+        this.ARBI_CLIENT_3_WALLET_ADDRESS = ensure('ARBI_CLIENT_3_WALLET_ADDRESS');
+        this.ARBI_CLIENT_3_RT_ASSET_ID = ensure('ARBI_CLIENT_3_RT_ASSET_ID');
+        this.ARBI_CLIENT_3_BACEN_ASSET_ID = ensure('ARBI_CLIENT_3_BACEN_ASSET_ID');
 
         this.STN_CNPJ8 = ensure('STN_CNPJ8');
         this.BRADESCO_CNPJ8 = ensure('BRADESCO_CNPJ8');
@@ -103,9 +125,11 @@ export class ConfigService {
         this.INTER_CNPJ8 = ensure('INTER_CNPJ8');
         this.ITAU_CNPJ8 = ensure('ITAU_CNPJ8');
         this.RIBEIRAO_PRETO_CNPJ8 = ensure('RIBEIRAO_PRETO_CNPJ8');
+
         this.TPFT_OPERATION_ID = ensure('TPFT_OPERATION_ID');
         this.LTN_ID = ensure('LTN_ID');
         this.LFT_ID = ensure('LFT_ID');
+
         this.ADDRESS_DISCOVERY_ADDRESS = ensure('ADDRESS_DISCOVERY_ADDRESS');
         this.TPFT_ADDRESS = ensure('TPFT_ADDRESS');
         this.TPFT_1002_ADDRESS = ensure('TPFT_1002_ADDRESS');
@@ -115,7 +139,7 @@ export class ConfigService {
         this.REAL_DIGITAL_ADDRESS = ensure('REAL_DIGITAL_ADDRESS');
         this.REAL_DIGITAL_DEFAULT_ACCOUNT_ADDRESS = ensure('REAL_DIGITAL_DEFAULT_ACCOUNT_ADDRESS');
         this.REAL_DIGITAL_ENABLE_ACCOUNT_ADDRESS = ensure('REAL_DIGITAL_ENABLE_ACCOUNT_ADDRESS');
-        // this.ARBI_REAL_TOKENIZADO_ADDRESS = ensure('ARBI_REAL_TOKENIZADO_ADDRESS');
+        this.REAL_TOKENIZADO_ARBI_ADDRESS = ensure('REAL_TOKENIZADO_ARBI_ADDRESS');
         this.STR_ADDRESS = ensure('STR_ADDRESS');
         this.SWAP_ONE_STEP_ADDRESS = ensure('SWAP_ONE_STEP_ADDRESS');
         this.SWAP_ONE_STEP_FROM_ADDRESS = ensure('SWAP_ONE_STEP_FROM_ADDRESS');
